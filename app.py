1c5f79d48fe0a741a1b8844ab390ab36813a0f52
darakere <deepthieduc@gmail.com>
2026-04-29 00:34:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ with tab1:
 
     # ================= SLEEP =================
     st.markdown('<div class="section-card sleep">', unsafe_allow_html=True)
-    with st.expander("🛌 ನಿದ್ದೆ.........."):
+    with st.expander("🛌 ನಿದ್ದೆ.....zZZ"):
 
         c1, c2 = st.columns(2)
 
